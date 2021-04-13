e3297526f367b312ef6d01c9d4922f8d6a1c693c
OmkarBhede <omkarbhede@gamil.com>
2021-04-13 10:19:46 +0530

    scoreboard added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var dino = document.getElementById("dino");
 var cactus = document.getElementById("cactus");
+var score = document.getElementById("score");
 
 dino.addEventListener("click", () => {
   if (dino.classList != "animate") {
@@ -9,6 +10,10 @@ dino.addEventListener("click", () => {
     dino.classList.remove("animate");
   }, 500);
 });
+var startTime;
+var endTime;
+
+start();
 
 var computeCollision = setInterval(function () {
   var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
@@ -18,6 +23,21 @@ var computeCollision = setInterval(function () {
   if (cactusLeft < 30 && cactusLeft > 0 && dinoTop >= 280) {
     cactus.style.animation = "none";
     cactus.style.display = "none";
-    alert("Arrrr. Try Again ") ? "" : location.reload();
+    alert("Arrrr. Try Again " + "[ score : " + scorecalc() + " ]")
+      ? ""
+      : location.reload();
   }
+  score.innerText = "score : " + scorecalc();
 }, 10);
+
+function start() {
+  startTime = new Date();
+}
+
+function scorecalc() {
+  endTime = new Date();
+  var timeDiff = endTime - startTime;
+  timeDiff /= 100;
+  var insecond = Math.round(timeDiff);
+  return insecond;
+}
